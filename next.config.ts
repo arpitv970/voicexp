8a655580d6792761bb6363ba91342b151c08ca18
Arpit Verma <arpitv970@gmail.com>
2025-01-09 15:58:44 +0530

    updated user context

--- next.config.ts
+++ next.config.ts
@@ -2,14 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "partnermarketinghub.withgoogle.com",
-        port: "", // Leave blank for default ports
-        pathname: "/**", // Allow all paths
-      },
-    ],
     domains: [
       "lh3.googleusercontent.com",
       "partnermarketinghub.withgoogle.com",
